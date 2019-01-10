ca9660751ac8a3fcc5516b114943947266b2ae52
Nicolas van Hell <45185574+nvanhell@users.noreply.github.com>
2019-01-09 19:52:40 -0500

    Update main.py

--- MacroSim/main.py
+++ MacroSim/main.py
@@ -12,14 +12,15 @@ class Simulation(threading.Thread):
     def __init__(self):
         self.simulation_on = False
         self.simulation_speed = 1  # Number of iterations before graphs get updated
-        self.timer = 0  # used to keep track of the time it takes to run a period (debugging)
+        self.timer = 0  # Used to keep track of the time it takes to run a period (for debugging)
         self.period = 0  # Variable for the current time period of the simulation
-        self.months_per_period = 1  # Number of months a worker will age per period
+        self.months_per_period = 1  # Number of months the simulation will advance per period
         self.retirement_age = 65  # Age at which a worker retires
         self.age_to_die = 80  # Age at which a worker gets deleted and replaced with a new worker
+        # Lists of the graph labels
         self.macro_labels = ['GDP', 'Consumption', 'Investment', 'Unemployment', 'Population']
         self.good_labels = ['Price', 'Quantity']
-        # The following are variables used to store economic variables so that they can be graphed in any period
+        # The following are variables used to store economic variables so that they can be graphed
         self.x = []
         self.y_macro = [[[], []] for i in range(len(self.macro_labels))]
         self.y_price = [[[], []] for i in range(economy.NUMBER_OF_GOODS)]
@@ -46,11 +47,13 @@ class Simulation(threading.Thread):
 
                 econ.price_vector = [market.get_price() for market in econ.markets]  # Updating price vector
                 for worker in econ.workers:
-                    worker.age += self.months_per_period / 12
+                    worker.age += self.months_per_period / 12  # Each worker ages a few months
                     if worker.age >= self.age_to_die:
-                        worker.kill(worker.b)  # Passes through the old utility function parameters to the new worker
+                        worker.kill(worker.b)  # Worker dies; passes through the old utility function parameters to the new worker
                     else:
+                        # If the worker stays alive, then update his/her individual demand for consumption goods
                         indiv_good_demand = worker.get_good_demand(econ.price_vector)
+                        # And aggregate demand for each good is the sum over all workers' demand
                         for i, market in enumerate(econ.markets):
                             market.quantity_demanded += indiv_good_demand[i]
 
@@ -63,6 +66,8 @@ class Simulation(threading.Thread):
                 """
 
                 # 2: Getting optimal allocation of L, K ----------------------------------------------------------------
+
+                # Currently unfinished. Missing a mechanism in which firms target optimal values of labour and capital
                 for market in econ.markets:
                     for firm in market.firms:
                         L, K = firm.cost_min_solution(econ.interest_rate).x
@@ -126,6 +131,8 @@ class Simulation(threading.Thread):
                 self.y_quantity[i][econ.id].append(econ.markets[i].quantity_sold)
 
     def display_graphs(self):
+        # Re-draws the graphs on the GUI for both economies
+        # to do: Clean this part up, it's messy
         if frame.tool_bar.graph_type == 0:
             for i, graph in enumerate(frame.two_graph_panel.graphs):
                 graph.draw(self.macro_labels[frame.tool_bar.graph_number],
@@ -142,6 +149,7 @@ class Simulation(threading.Thread):
 
 
 def create_main_frame(pop, number_of_goods, number_of_firms):
+    # Initializes both economies and created the main GUI frame
     global frame
     global economies
     global simulation
